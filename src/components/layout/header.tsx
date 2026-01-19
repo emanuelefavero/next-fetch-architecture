227b917f2c38a4f6fd8d81eb60cffafc2f713c3d
Emanuele Favero <emafaux89@gmail.com>
2026-01-19 20:48:19 +0100

    feat: add metadata config

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,10 @@
+import { TITLE } from '@/config/metadata'
 import { ModeToggle } from '@/features/theme/components/mode-toggle'
 
 export function Header() {
   return (
     <header className='py-py flex items-center justify-between gap-2 border-b border-border px-px'>
-      <div className='font-bold'>Next Fetch Architecture</div>
+      <div className='font-bold'>{TITLE}</div>
       <div className='flex items-center gap-4'>
         <ModeToggle />
       </div>
---
+++ src/config/metadata.ts
@@ -0,0 +1,8 @@
+export const TITLE = 'Next Fetch Architecture'
+export const DESCRIPTION =
+  'A Next.js application demonstrating a type-safe data fetching architecture'
+
+export const metadata = {
+  title: TITLE,
+  description: DESCRIPTION,
+} as const
