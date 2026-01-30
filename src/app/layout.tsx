d7ced751ed8e74a9ea9e91fcf1d1259ddab929f0
Emanuele Favero <emafaux89@gmail.com>
2026-01-30 19:19:09 +0100

    chore: use system fonts instead of Geist fonts throughout the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,7 @@ import { Main } from '@/components/layout/main'
 import { metadata as appMetadata } from '@/config/metadata'
 import { ThemeProvider } from '@/features/theme/components/theme-provider'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   ...appMetadata,
@@ -27,9 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className='antialiased'>
         <ThemeProvider
           attribute='class'
           defaultTheme='system'
