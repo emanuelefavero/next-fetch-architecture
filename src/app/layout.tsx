34333d7492528c6f20547e555fda4f55dc0048f0
Emanuele Favero <emafaux89@gmail.com>
2026-02-04 02:29:57 +0100

    feat: center body content horizontally at screen-2xl breakpoint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en' suppressHydrationWarning>
-      <body>
+      <body className='mx-auto w-full max-w-screen-2xl'>
         <ThemeProvider
           attribute='class'
           defaultTheme='system'
