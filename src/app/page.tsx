5600807b5f4929f50898c1198945e57f676efe74
Emanuele Favero <emafaux89@gmail.com>
2026-02-05 21:07:23 +0100

    feat: install shadcn/ui form components and react-hook-form
    - Installed Shadcn/ui form components
    - Installed react-hook-form for form handling
    - Added CreateUserForm component boilerplate
      and integrated it into the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { TypographyH1 } from '@/components/typography/h1'
+import CreateUserForm from '@/features/users/components/create-user/form'
 import { UsersLoader } from '@/features/users/components/users-list/loader'
 import { UsersListSkeleton } from '@/features/users/components/users-list/skeleton'
 import type { SearchParams } from '@/types/routing'
@@ -12,6 +13,8 @@ export default function Home({ searchParams }: PageProps) {
   return (
     <>
       <TypographyH1>Users</TypographyH1>
+
+      <CreateUserForm />
 
       <Suspense fallback={<UsersListSkeleton />}>
         <UsersLoader searchParams={searchParams} />
---
+++ src/features/users/components/create-user/form.tsx
@@ -0,0 +1,3 @@
+export default function CreateUserForm() {
+  return <div>CreateUserForm</div>
+}
